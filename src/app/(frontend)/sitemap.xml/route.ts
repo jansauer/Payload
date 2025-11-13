12855aa04cd1df93ab442f98b936e69090a0104b
Jan Sauer <jan@jansauer.de>
2025-11-13 07:59:12 +0100

    Add a sitemap

---
+++ src/app/(frontend)/sitemap.xml/route.ts
@@ -0,0 +1,35 @@
+import { SITE_URL } from '@/site'
+import { payload } from '../../../payload';
+
+export async function GET() {
+  const stories = await payload.find({
+    collection: 'stories',
+    where: {
+      _status: {
+        not_equals: 'draft',
+      },
+    },
+    limit: 50,
+  });
+
+  const urls = stories.docs.map(
+    (story) =>
+      "  <url>" +
+      `<loc>${SITE_URL}${story.nanoid}</loc>` +
+      `<lastmod>${story.publishedAt ?? story.createdAt}</lastmod>` +
+      "</url>",
+  );
+
+  const lastmod = stories.docs[0].publishedAt ?? stories.docs[0].createdAt;
+
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>${SITE_URL}</loc><lastmod>${lastmod}</lastmod></url>
+${urls.join("\n")}
+</urlset>`, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
---
+++ src/site.ts
@@ -0,0 +1,6 @@
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://jansauer.de/';
+
+// const SITE_URL =
+//   process.env.NEXT_PUBLIC_SERVER_URL ||
+//   process.env.VERCEL_PROJECT_PRODUCTION_URL ||
+//   'https://example.com'
